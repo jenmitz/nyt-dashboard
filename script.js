80a47ea1220790393c42529a620776d69c0b7c98
Drubaloo <Drubaloo@outlook.com>
2020-02-04 21:28:47 -0800

    added funtionality

--- script.js
+++ script.js
@@ -1,4 +1,52 @@
-// reset form
-$('#clear').click(function(){
-    $('#')[0].reset();
-});
+// page Elements
+var searchInputEL = $(`#search`)
+var numberInputEL = $(`#number`)
+var starYearEL = $(`#start-year`)
+var endYearEl = $('#end-year')
+var searchEl = $('#submit')
+var clearEL = $(`#clear`)
+var resultEL = $(`#result`)
+
+//url for API call
+var searchTerm = searchInputEL.val()
+
+var APIKey = "&api-key=QS1sj6OrbFwvBFp89VHDEa5XirpGVe6M";
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + APIKey;
+
+//funtion for getting info from api
+function searchButton() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < numberInputEL.val(); i++) {
+        
+        console.log(response.response.docs[i])
+
+        var title = response.response.docs[i].headline.main
+
+        var tOne = $(`<p>`).text(title)
+
+        resultEL.append(tOne)
+    }
+
+    })
+    
+}
+
+//submit btn
+
+$('#submit').on('click', function () {
+    
+    searchButton() 
+})
+
+// reset button
+$('#clear').on('click', function () {
+    console.log("hello")
+    window.location.reload()
+})
+
+
+
